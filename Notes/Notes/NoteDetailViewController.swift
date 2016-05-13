1b93a5b491b9477a63d6714e67423b211de69de8
Ross McIlwaine <rdm0423@gmail.com>
2016-05-13 10:23:12 -0600

    Saves Notes, Loads and Displays

--- Notes/Notes/NoteDetailViewController.swift
+++ Notes/Notes/NoteDetailViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class NoteDetailViewController: UIViewController {
+class NoteDetailViewController: UIViewController, UITextFieldDelegate {
 
+    var note: Note?
+    
+    @IBOutlet weak var noteTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // set title of detail to be first ... characters of the note
+        
+        if let note = note {
+            
+            updateWithNote(note)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        
+        if let note = self.note {
+            
+            note.entry = noteTextView.text
+            note.timeStamp = NSDate()
+        } else {
+            
+            let newNote = Note(entry: self.noteTextView.text)
+            NoteController.sharedController.addNote(newNote)
+            self.note = newNote
+        }
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func updateWithNote(note: Note) {
+        
+        self.note = note
+        
+        noteTextView.text = note.entry
+        
+        self.navigationItem.title = note.entry
+    }
+    
+    
+    // MARK: UITextField Dismiss
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
     
 
     /*
--- Notes/Notes/NoteTableViewController.swift
+++ Notes/Notes/NoteTableViewController.swift
@@ -19,6 +19,12 @@ class NoteTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        tableView.reloadData()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,23 +35,26 @@ class NoteTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return NoteController.sharedController.notes.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCellWithIdentifier("noteCell", forIndexPath: indexPath)
+        
+        let note = NoteController.sharedController.notes[indexPath.row]
+        
+        cell.textLabel?.text = note.entry
+        cell.detailTextLabel?.text = "\(note.timeStamp)"
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
@@ -55,41 +64,37 @@ class NoteTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            
+            let note = NoteController.sharedController.notes[indexPath.row]
+            NoteController.sharedController.removeNote(note)
+            
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "viewNoteSegue" {
+            
+            let detailViewController = segue.destinationViewController as? NoteDetailViewController
+            
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let note = NoteController.sharedController.notes[indexPath.row]
+                detailViewController?.note = note
+            }
+        }
     }
-    */
-
 }
